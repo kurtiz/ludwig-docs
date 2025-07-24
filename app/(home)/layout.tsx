fb0dfed7383a25ecd555cf6ccf4d9291ecb7cc90
Aaron Will Djaba <papiliocurtis@gmail.com>
2025-07-24 08:41:32 +0000

    Add button component, new styles, and utilities
    Introduced a reusable `Button` component with multiple variants and sizes, utilizing utility functions for styling. Updated global styles with new theme variables and added dependencies like `lenis` and `lucide-react` for additional functionality. Enhanced layout files for home and docs with nested wrappers and smooth scrolling support.

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,7 +1,12 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+import type {ReactNode} from 'react';
+import {HomeLayout} from 'fumadocs-ui/layouts/home';
+import {baseOptions} from '@/app/layout.config';
+import ReactLenis from 'lenis/react';
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+export default function Layout({children}: { children: ReactNode }) {
+    return <HomeLayout {...baseOptions}>
+        <ReactLenis root>
+            {children}
+        </ReactLenis>
+    </HomeLayout>;
 }
--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,4 +1,4 @@
-import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import type {BaseLayoutProps} from 'fumadocs-ui/layouts/shared';
 
 /**
  * Shared layout configurations
@@ -8,21 +8,29 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  nav: {
-    title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
-      </>
-    ),
-  },
-  // see https://fumadocs.dev/docs/ui/navigation/links
-  links: [],
+    nav: {
+        title: (
+            <>
+                <img
+                    src="/logo-light.png"
+                    alt="Fumadocs Logo"
+                    className="w-18 hidden dark:block"
+                />
+                <img
+                    src="/logo-dark.png"
+                    alt="Fumadocs Logo"
+                    className="w-18 dark:hidden block"
+                />
+            </>
+        ),
+    },
+    githubUrl: 'https://github.com/ludwig-framework/ludwig',
+    // see https://fumadocs.dev/docs/ui/navigation/links
+    links: [
+        {
+            text: '',
+            url: '/',
+            active: 'nested-url',
+        },
+    ],
 };
